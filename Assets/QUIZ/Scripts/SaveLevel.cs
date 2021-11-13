1ec37e6f2abdab804a0db068c603dc42ae5c4a18
CacaAlves <cacazinho99@outlook.com>
2021-11-13 19:09:45 -0300

    First part done (of the last assignment)

---
+++ Assets/QUIZ/Scripts/SaveLevel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+public class SaveLevel
+{
+    public static void Write(string text)
+    {
+        string path = "Assets/QUIZ/nivel.txt";
+        StreamWriter writer = new StreamWriter(path, false);
+        writer.WriteLine(text);
+        writer.Close();
+    }
+    public static string Get()
+    {
+        string path = "Assets/QUIZ/nivel.txt";
+        StreamReader reader = new StreamReader(path); 
+        string nivel = reader.ReadToEnd();
+        reader.Close();
+
+        return nivel;
+    }
+}
---
+++ Assets/Scripts/SetFullScreenOnLoad.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetFullScreenOnLoad : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public bool fullScreen;
+
+    void Start()
+    {
+        Screen.fullScreen = fullScreen;
+    }
+}
